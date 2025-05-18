711b9e2e811e845584c982f5a150086572361205
Goh Kun Shun (KHUN) <kunshun225@gmail.com>
2025-05-19 02:17:58 +0800

    Add scripts to train/test with diam.

---
+++ test_diam.sh
@@ -0,0 +1,28 @@
+export CUBLAS_WORKSPACE_CONFIG=:16:8 # to eliminate LSTM non-deterministic
+
+if [[ "$1" == "--link_only" ]]
+then
+    prefix=diam_link_only_checkpoints
+else
+    prefix=diam_checkpoints
+fi
+
+GPU_ID=0
+
+for seed in 1
+do
+  for lr in 2e-5
+  do
+    for epochs in 6
+    do
+        python main.py --encoder_model "$prefix"/"$seed"_"$lr"_"$epochs"/ \
+          --seed $seed \
+          --gpu $GPU_ID \
+          --data_dir data/diam \
+          --max_num_test_contexts 37 \
+          --eval \
+          --fp16 \
+          $1
+    done
+  done
+done
