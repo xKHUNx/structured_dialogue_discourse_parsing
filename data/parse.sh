1de89e14f0fc4eedfa9fa16327255ac2ac488758
Goh Kun Shun (KHUN) <kunshun225@gmail.com>
2026-01-23 13:06:46 +0800

    Revert to 20 EDUs only

--- data/parse.sh
+++ data/parse.sh
@@ -2,12 +2,12 @@ python build_relation_database.py --dir_path diam
 python build_relation_database.py --dir_path stac
 cp stac/relation_database.json molweni/relation_database.json
 
-python parse.py --dir_path diam --num_contexts 774 $1
+python parse.py --dir_path diam --num_contexts 20 $1
 python parse.py --dir_path stac --num_contexts 20 $1
 python parse.py --dir_path molweni --num_contexts 14 $1
 
-python parse_test.py --dir_path diam --num_contexts 774 --mode dev $1
-python parse_test.py --dir_path diam --num_contexts 774 $1
+python parse_test.py --dir_path diam --num_contexts 20 --mode dev $1
+python parse_test.py --dir_path diam --num_contexts 20 $1
 python parse_test.py --dir_path stac --num_contexts 37 $1 --mode dev $1
 python parse_test.py --dir_path stac --num_contexts 37 $1
 python parse_test.py --dir_path molweni --num_contexts 14 --mode dev $1
